7b736feb1eccbf3a7b8f816dcdfde5311c8c446b
Lee Zamastil <l.zamastil@gmail.com>
2016-09-06 15:40:35 -0700

    Added sections and data attributes to html

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Portfolio Assignment
 
 Author: Lee Zamastil
-Assignment from Class 01 | Code Fellows 301
+Assignment from Class 02 | Code Fellows 301
 
 Begin with valid and semantic markup. Link to social media and GitHub profiles.
+
+Use bubbling to handle event clicking on nav links.
--- scripts/article.js
+++ scripts/article.js
@@ -2,9 +2,23 @@
 
 var articles = [];
 
-function Article (selects) {
+function Article (selects) {    
     this.title = selects.title;
     this.category = selects.category;
     this.body = selects.body;
     this.publishedOn = selects.publishedOn;
 }
+
+// Get the element, add a click listener...
+document.getElementById("parent-list").addEventListener("click", function(e) {
+    // e.target is the clicked element!
+	// If it was a list item
+	if(e.target && e.target.nodeName == "LI") {
+        // List item found!  Output the ID!
+        console.log("List item ", e.target.id.replace("post-", ""), " was clicked!");
+	}
+});
+
+$("nav-list").on('click', function(e) {
+
+});
