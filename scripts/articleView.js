24e5c98eb9d29a5e6932dbb9e40b8ed1d74b270c
Lee Zamastil <l.zamastil@gmail.com>
2016-09-06 16:01:26 -0700

    handleMainNav method called and properly executed

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -1,18 +1,26 @@
 
 
+// Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
+var articleView = {};
+
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav element that will power the Tabs feature.
-  //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
-  //       single .tab-content section that is associated with the clicked .tab element.
-  //       So: You need to dynamically build a selector string with the correct ID, based on the
-  //       data available to you on the .tab element that was clicked.
-  //       DONE: Drew & Lee
-  $('.main-nav .tab').on('click', function() {
-    var tab_id = $(this).attr('data-content');
-    var selectedTab = $('#' + tab_id);
-    $('section.tab-content').hide();
-    selectedTab.show();
-  });
-  $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+    // TODO: Add an event handler to .main-nav element that will power the Tabs feature.
+    //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
+    //       single .tab-content section that is associated with the clicked .tab element.
+    //       So: You need to dynamically build a selector string with the correct ID, based on the
+    //       data available to you on the .tab element that was clicked.
+    //       DONE: Drew & Lee
+    $('.main-nav .tab').on('click', function() {
+        var tab_id = $(this).attr('data-content');
+        var selectedTab = $('#' + tab_id);
+        $('section.tab-content').hide();
+        selectedTab.show();
+    });
+    $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
+
+// Call the function on document load
+$(document).ready(function() {
+    articleView.handleMainNav();
+});
